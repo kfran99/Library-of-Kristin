c1869a6fe7c6dbb4a42cfbf6739dd74c38a138c2
Kristin Frandsen <KFran@Kristins-MacBook-Air.local>
2014-03-17 16:20:31 -0700

    Still need to figure out how to pull the data from what I get from Amazon.

--- search_amazon.py
+++ search_amazon.py
@@ -1,11 +1,22 @@
 import amazonproduct
+from lxml import etree as ET
 
 api=amazonproduct.API(cfg=".amazon-product-api", locale="us")
 
-def get_book_by_title(title):
-	items = api.item_search("Books", Title=title, ItemPage=1)
-	for item in items:
-		print unicode(item.ItemAttributes.Title)
+def get_book_by_title_author(title, author):
+	books = api.item_search("Books", Title=title, 
+									Author=author, 
+									ItemPage=1)
+	#title = books.ItemAttributes.Title
+	#author = books.ItemAttributes.Author
+	for book in books:
+		print "%s: %s" % (book.ItemAttributes.Title, book.ItemAttributes.Author)
+		#prints out more details - need to figure out how to get what I want
+		#look at http://stackoverflow.com/questions/13093091/lxml-or-lxml-html-print-tree-structure
+		print ET.tostring(book, pretty_print=True)
 
-get_book_by_title("Little Women") 
+		#return (title, author, pages, description, url for Amazon link to book)
+#Eventually want this to go to a template so I can pick the best result for my database and then add it.
+#return render_template("amazon_results.html", )		
 
+get_book_by_title_author("The Time Traveler's Wife", "Audrey Niffenegger") 
