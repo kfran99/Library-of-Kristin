34e6326edd4d8258d54f229ef7858bd711d77c28
Kristin Frandsen <KFran@Kristins-MacBook-Air.local>
2014-03-18 17:52:25 -0700

    Figured out how to get image and description from the Amazon API. Need to link all the functions now and start building database of books.

--- search_amazon.py
+++ search_amazon.py
@@ -1,22 +1,41 @@
 import amazonproduct
-from lxml import etree as ET
+#from lxml import etree as ET
+#from lxml import objectify
 
 api=amazonproduct.API(cfg=".amazon-product-api", locale="us")
 
 def get_book_by_title_author(title, author):
-	books = api.item_search("Books", Title=title, 
-									Author=author, 
-									ItemPage=1)
-	#title = books.ItemAttributes.Title
-	#author = books.ItemAttributes.Author
+#http://docs.aws.amazon.com/AWSECommerceService/latest/DG/ItemSearch.html	
+	books = api.item_search("Books", Title=title,
+		                             Author=author,
+		                             ItemPage=1)
+
 	for book in books:
-		print "%s: %s" % (book.ItemAttributes.Title, book.ItemAttributes.Author)
-		#prints out more details - need to figure out how to get what I want
-		#look at http://stackoverflow.com/questions/13093091/lxml-or-lxml-html-print-tree-structure
-		print ET.tostring(book, pretty_print=True)
+		book = book.ItemAttributes.Title, book.ItemAttributes.Author, book.DetailPageURL, book.ASIN
 
-		#return (title, author, pages, description, url for Amazon link to book)
-#Eventually want this to go to a template so I can pick the best result for my database and then add it.
-#return render_template("amazon_results.html", )		
-
+		print unicode(book)
+		#print "%s: %s, %s, %s" % (book.ItemAttributes.Title,
+		#	                      book.ItemAttributes.Author,
+		#	                      book.DetailPageURL,
+		#	                      book.ASIN)
+		#return render_template("amazon_results.html", )
+		#print ET.tostring(book, pretty_print=True)		
 get_book_by_title_author("The Time Traveler's Wife", "Audrey Niffenegger") 
+
+
+def get_book_image(asin):
+#to get image for specific book by ASIN
+#http://docs.aws.amazon.com/AWSECommerceService/latest/DG/CHAP_response_elements.html#LargeImage	
+	book_image = api.item_lookup(ItemId=asin, IDType="ASIN", ResponseGroup="Images")
+	image = book_image.Items.Item.ImageSets.ImageSet.LargeImage.URL
+	print image
+get_book_image("015602943X")
+
+
+def get_book_description(asin):
+#to get description from editorial review 
+#can't just get description - may be copyrighted per research
+	editorial_review = api.item_lookup(ItemId=asin, IDType="ASIN", ResponseGroup="EditorialReview")
+	book_description = editorial_review.Items.Item.EditorialReviews.EditorialReview.Content
+	print book_description
+get_book_description("015602943X")		
