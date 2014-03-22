5137299fa0a02a7ce3e40f967fe4165e05dd3320
Kristin Frandsen <KFran@Kristins-MacBook-Air.local>
2014-03-21 17:38:55 -0700

    Working on adding book to database.

--- app/controller.py
+++ app/controller.py
@@ -4,7 +4,6 @@ from model import *
 from forms import RegistrationForm, AmazonSearch
 from search_amazon import get_book_by_title_author, get_book_info
 
-
 @app.route("/")
 @app.route("/index")
 def index():
@@ -22,37 +21,27 @@ def new_user_form():
 		return redirect("/index")
 	return render_template("new_user_form.html", form=form)
 
-	#return render_template("new_user_form.html")
-
-
-# @app.route("/user/new", methods=["POST"])
-# def new_user():
-# 	form = RegistrationForm()
-# 	if form.validate_on_submit():
-# 		flash ("Your account has been created=" + form.given_name.data)		
-# 		return redirect("/index")
-# 	return render_template("new_user_form.html", form=form)
-
 @app.route("/book/search", methods=["GET", "POST"])
 def amazon_search():
-	form = AmazonSearch()	
+	form = AmazonSearch()
 	if form.validate_on_submit():
 		flash ("Information supplied is complete.")
 		books = get_book_by_title_author(form.title.data, form.author.data)
-		for book in books:
-			title = book.ItemAttributes.Title
-			author = book.ItemAttributes.Author
-			link_to_amazon = book.DetailPageURL
-			asin = book.ASIN
-			#asin can be used as an ID
-		#book = book.ItemAttributes.Title, book.ItemAttributes.Author, book.DetailPageURL, book.ASIN
-			print unicode(title), unicode(author), unicode(link_to_amazon), unicode(asin)			
-		return redirect("/index")
+		#get_book_by_title_author is defined in search_amazon
+		return render_template("amazon_results.html", amazon_res=books)
+	
 	return render_template("amazon_search.html", form=form)
 
-
-
-
-
-
+@app.route("/book/add", methods=["GET"])
+def add_book():
+	asin = request.args.get("asin")
+	title = request.args.get("title")
+	author = request.args.get("author")
+	image_url, description, genre = get_book_info(asin)
+	print asin, title, author, image_url, description, genre
+	# genre = get_book_info(book_genre.Items.Item.BrowseNodes.BrowseNode.Name)
+	# description = get_book_info(editorial_review.Items.Item.EditorialReviews.EditorialReview.Content)
+	# image_url = get_book_info(image_url.Items.Item.ImageSets.ImageSet.LargeImage.URL)
+	#amazon_url = request.args.get("DetailPageURL")
+	return render_template("view_added_book.html")
 
--- app/search_amazon.py
+++ app/search_amazon.py
@@ -5,44 +5,31 @@ from lxml import objectify
 api=amazonproduct.API(cfg=".amazon-product-api", locale="us")
 
 def get_book_by_title_author(title, author):
-#http://docs.aws.amazon.com/AWSECommerceService/latest/DG/ItemSearch.html	
+#http://docs.aws.amazon.com/AWSECommerceService/latest/DG/ItemSearch.html
 	books = api.item_search("Books", Title=title, 
-		                             Author=author)
+		                             Author=author,
+		                             ItemPage=1)
 	return books
-	#this is going to happen in my controller and iterate through Jinja
-	for book in books:
-		title = book.ItemAttributes.Title
-		author = book.ItemAttributes.Author
-		link_to_amazon = book.DetailPageURL
-		asin = book.ASIN
-		#book = book.ItemAttributes.Title, book.ItemAttributes.Author, book.DetailPageURL, book.ASIN
-		print unicode(title), unicode(author), unicode(link_to_amazon), unicode(asin)		
-		#return render_template("amazon_results.html", )
-		#print ET.tostring(book, pretty_print=True)		
-get_book_by_title_author("The Historian", "Elizabeth Kostova") 
 
 
 def get_book_info(asin):
+	print asin , "asin"
 	#to get image for specific book by ASIN
 	#http://docs.aws.amazon.com/AWSECommerceService/latest/DG/CHAP_response_elements.html#LargeImage	
-	book_image = api.item_lookup(ItemId=asin, 
+	image_url = api.item_lookup(ItemId=asin, 
 		                         IdType="ASIN", 
 		                         ResponseGroup="Images")
-	image = book_image.Items.Item.ImageSets.ImageSet.LargeImage.URL
-	print image
+
 	#to get description from editorial review
 	#http://docs.aws.amazon.com/AWSECommerceService/latest/DG/RG_EditorialReview.html 
 	editorial_review = api.item_lookup(ItemId=asin, 
 		                               IdType="ASIN", 
 		                               ResponseGroup="EditorialReview")
-	book_description = editorial_review.Items.Item.EditorialReviews.EditorialReview.Content
-	print unicode(book_description)
 	#For more info - http://docs.aws.amazon.com/AWSECommerceService/latest/DG/CHAP_response_elements.html	
 	book_genre = api.item_lookup(ItemId=asin, 
 		                         IdType="ASIN",
 		                         ResponseGroup="BrowseNodes")
-	genre = book_genre.Items.Item.BrowseNodes.BrowseNode.Name
-	print genre
-get_book_info("0316070637")
-
-
+	print book_genre.Items.Item.BrowseNodes.BrowseNode.Name
+	print unicode(editorial_review.Items.Item.EditorialReviews.EditorialReview.Content)
+	print image_url.Items.Item.ImageSets.ImageSet.LargeImage.URL
+	return image_url, editorial_review, book_genre
